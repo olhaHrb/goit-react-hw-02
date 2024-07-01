30b6a7a4b5ee2331ba9a76714a990aa8fa2a1b41
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-07-01 15:38:48 +0300

    new step 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Description from "../Description/Description";
 import Options from "../Options/Options";
 import Feedback from "../Feedback/Feedback";
-//import Notification from "../Notification/Notification";
+import Notification from "../Notification/Notification";
 
 const App = () => {
   const state = {
@@ -19,11 +19,17 @@ const App = () => {
     });
   };
 
+  const totalFeedback = clicks.good + clicks.neutral + clicks.bad;
+
   return (
     <div>
       <Description />
       <Options updateFeedback={updateFeedback}> </Options>
-      <Feedback value={clicks}></Feedback>
+      {totalFeedback > 0 ? (
+        <Feedback value={clicks}></Feedback>
+      ) : (
+        <Notification></Notification>
+      )}
     </div>
   );
 };
